506ab86ba2aa22be635adc8c3ae22dd8a2a9d877
ZeTta062 <samhak17@naver.com>
2025-01-06 11:09:25 +0900

    alpha test

--- server.js
+++ server.js
@@ -6,12 +6,35 @@ import viteExpress from "vite-express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
-io.on('connection', () => { /* _ */ });
+io.on('connection', (client) => { 
+    console.log("사용자가 들어왔습니다!");
+    console.log(client.handshake.query);
+
+    const connectedClientUsername = client.handshake.query.username;
+
+    console.log(`사용자가 들어왔습니다! ${connectedClientUsername}`);
+
+    client.broadcast.emit("new_message", {username : "관리자", message: `${connectedClientUsername}님이 방에 들어왔습니다.`})
+
+    client.on('new_message', (msg) => {
+        io.emit("new_message", {username : msg.username, message: msg.message})
+    })
+
+    client.on('disconnect', () => {
+        console.log(`사용자가 나갔습니다... ${connectedClientUsername}`);
+        io.emit("new_message", {username : "관리자", message: `${connectedClientUsername}님이 방에서 떠났습니다.`})
+    })
+ });
+
 
 server.listen(3000, () => {
-    console.log("서버에서 듣고 있습니다.. 3000")
+    console.log("서버에서 듣고 있습니다.. 3000");
 });
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// ES modules
+import { io } from "socket.io-client";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isConnected, setIsConnected] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [frontSocket, setFrontSocket] = useState(null);
+
+  function connectToChatServer() {
+    console.log('connectToChatServer');
+    const _frontSocket = io('http://localhost:3000', {
+      autoConnect : false,
+      query: {
+        username: username,
+      },
+    });
+    _frontSocket.connect();
+    setFrontSocket(_frontSocket);
+  }
+  
+  function disconnectToChatServer() {
+    console.log('disconnectToChatServer');
+    frontSocket?.disconnect();
+  }
+
+  function onConnected() {
+    console.log("front on Connected");
+    setIsConnected(true);
+  }
+
+  function onDisconnected() {
+    console.log("front on Disconnected");
+    setIsConnected(false);
+  }  
+
+  function onMessageReceived(msg) {
+    console.log("onMessageReceived");
+    console.log(msg);
+
+    setMessages(previous => [...previous, msg]);
+  }
+
+  function sendMessageToChatServer() {
+    console.log(`front send Message To ChatServer / ${userInput}`);
+    frontSocket?.emit("new_message", {username: username, message: userInput}, (response) => {
+      console.log(response);
+    })
+  }  
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      left: 0,
+      behavior: "smooth"
+    })
+  }, [messages]);
+
+  useEffect(() => {
+    frontSocket?.on("connect", onConnected); 
+    frontSocket?.on("disconnect", onDisconnected); 
+    frontSocket?.on("new_message", onMessageReceived); 
+
+    return () => {
+      frontSocket?.off("connect", onConnected);
+      frontSocket?.off("disconnect", onDisconnected); 
+      frontSocket?.on("new_message", onMessageReceived); 
+    };
+  }, [frontSocket]);
+
+  const messageList = messages.map((aMsg, index) => 
+    <li key={index}>
+      {aMsg.username} : {aMsg.message}
+    </li>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='Main'>
+      <h1>유저: {username}</h1>
+      <h2>접속상태: {isConnected ? "🟢온라인" : "⚫오프라인"}</h2>
+      <div className='card'>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        <button onClick={() => connectToChatServer()}>
+            접속
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => disconnectToChatServer()}>
+          접속 종료
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className='card'>
+        <input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+        <button onClick={() => sendMessageToChatServer()}>
+          보내기
+        </button>
+      </div>
+      <ul>
+        {messageList}
+      </ul>
+    </div>
   )
 }
 
